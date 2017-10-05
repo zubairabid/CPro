d5b051af4660cd602d07759e1e34171d4b0cf695
Zubair Abid <zubairabid1999+github@gmail.com>
2017-10-05 22:31:51 +0530

    Not complete yet, 2_2

--- 2/2_2.c
+++ 2/2_2.c
@@ -1,50 +1,38 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <math.h>
 
-int main() {
-    int N, seg, i, j, flag, sum = 0;
-    scanf("%d", &N);
-    int a[N];
-    for(i = 0; i < N; i++) //array input
-        scanf("%d", &a[i]);
-
-    int freq[10];
-    if(N > 10)
-        seg = 10;
-    else
-        seg = N;
-
-    while(seg > 0) {
-        //printf("Segment length: %d\n", seg);
-        for(i = 0; i <= N-seg; i++) {//to iterate through all like lengthed subarrays
-
-            //printf("Iteration bounds: %d %d\n", i, (N-seg));
-            flag = 0;
-            for(j = 0; j < 10; j++)
-                freq[j] = 0;
-
-            for(j = i; j < i+seg; j++) {//iterates through subarray
-                //printf("%d ", a[j]);
-
-                if(freq[a[j]] == 0) {//this is fine
-                    freq[a[j]] = 1;
-                }
-                else {//this isn't. Abort
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag == 0) //If it's untouched
-                sum += seg;//Add
-
-            //printf("Sum: %d\n", sum);
-        }
-        seg--;
-    }
-
-    printf("%d\n", sum);
-
-    return 0;
+int A[100001];
+int fr[100001];
+int main(void) {
+	int i, N, l = 0, r = 0;
+	long long int sum = 0;
+	
+	scanf("%d", &N); //limit input
+	for(i = 0; i < N; i++) {//array input
+		scanf("%d", &A[i]);	
+	}
+	
+	while(r < N) {
+		//printf("Right pointer is at %d (left at %d)\n", r, l);
+		fr[A[r]]++;
+		//printf("%d exists. Frequency is %d\n", A[r], fr[A[r]]);
+		if(fr[A[r]] > 1) {
+			do {
+				fr[A[l]]--;
+				//printf("%d now existed %d times\n", A[l], fr[A[l]]);
+				l++;
+				//printf("Left pointer is now at %d\n", l);
+			} while(fr[A[r]] > 1);
+			sum--;
+			fr[A[r]] = 0;
+			r--;
+			l++;
+		}
+		else {
+			sum += ((r-l+1)*(r-l+2))/2;
+		}
+		r++;
+	}
+	
+	printf("%lld\n", sum);
+	return 0;
 }
-
