b3d034f4b05865cc94c4732e96abf28cedc00201
Zubair Abid <zubairabid1999+github@gmail.com>
2017-10-05 19:57:49 +0530

    2_1.c redone

--- 2/2_1.c
+++ 2/2_1.c
@@ -4,9 +4,10 @@
 
 int main() {
     int N, K, i, j, sum, c = 0;
-    int a[N], comp[N];
 
     scanf("%d %d", &N, &K);
+    
+    int a[N], comp[N];
     for(i = 0; i < N; i++) {
         scanf("%d", &a[i]);
     }
@@ -30,22 +31,14 @@ int main() {
         }
         printf("\n");*/
         if(sum == K) {//printing. May want to break;
-            c++;
-            if(c==1) {//to print it once
-                printf("Yes\n");
-            }
-            for(j = 0; j < N; j++) {
-                if(comp[j] == 1) {
-                    printf("%d ", a[j]);
-                }
-            }
-            printf("\n");
+	c = 1;
+            printf("YES\n");
+            break;
         }
     }
     if(c==0) {
-        printf("No\n");
+        printf("NO\n");
     }
 
     return 0;
 }
-
