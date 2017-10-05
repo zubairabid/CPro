6e0418ce93312249c21e88af3fc014e4968f2deb
Zubair Abid <zubairabid1999+github@gmail.com>
2017-10-05 21:04:21 +0530

    Solution for 2_9

--- 2/2_9.c
+++ 2/2_9.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
 //#include <stdlib.h>
-#include <math.h>
 
-int prime(int);
+char sieve[1000001]; // sieve[i] stores 0 if prime, 1 if composite. Global variable => all 0 by default
+long long sum[1000001];
 
 int main() {
-
     int i, j, T, n, m;
-    long long s = 0;
+    
+    sieve[0] = 1;
+    sieve[1] = 1;
+    
+    i = 2, j = 2<<1;	
+    for(i = 2; i <= 1001; i++) {
+        if(sieve[i] == 1) {
+            continue;
+        }
+        for(j = i<<1; j < 1000001; j+=i) {
+            sieve[j] = 1;
+        }
+    }
+    for(i = 0; i < 1000001; i++) {
+        sum[i] = sum[i-1];
+        if(sieve[i] == 0)
+    	sum[i]+=i;
+    }
+    
+    //for(i = 0; i < 1000001; i++) {
+    //    printf("%d:%lld,%d\n", i, sum[i], sieve[i]);
+
+    //}
+    
+    i=2;
     scanf("%d", &T);//Input testcase number
 
     for(i = 0; i < T; i++) {
-        scanf("%d %d", &m, &n); //Input Bounds
-        s = 0; // reset or bust
-
-        for (j = m; j <= n; j++) {
-            if(prime(j) == 1)
-                s+=j;
-        }
-        printf("%lld\n", s);
-
+        scanf("%d %d", &n, &m); //Input Bounds
+        printf("%lld\n", (sum[m]-sum[n-1]));
     }
     return 0;
 }
-
-int prime(int n) { //prime function, may need optimization
-    int i;
-    for(i = 2; i <= sqrt(n); i++) {
-        if(n%i==0)
-            return 0;
-    }
-
-    return n!=1?1:0;
-}
-
+		
