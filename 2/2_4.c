dbff69076fac42ad7316fae71ef29a78ff09fa02
Zubair Abid <zubairabid1999+github@gmail.com>
2017-10-06 16:29:29 +0530

    Solution for 2_4

--- 2/2_4.c
+++ 2/2_4.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 long long int div(long long int, long long int, long long int);
-
 int main(void) {
 	long long int X, Y, M, T, i;
 	scanf("%lld", &T);
 	
 	for(i = 0; i < T; i++) {
-		//printf("Working on test case %lld\n", (i+1));
 		scanf("%lld %lld %lld", &X, &Y, &M);
 		printf("%lld\n", div(X, Y, M));
 	}
@@ -16,16 +14,21 @@ int main(void) {
 
 long long int div(long long int X, long long int Y, long long int M) {
 	//printf("%lld p %lld mod %lld\n", X, Y, M);
-	if(Y==1) {
-	//	printf("(Base) Returning %lld\n", (X&M));
-		return (X%M);
+	long long int d = 1;
+	
+	d = X%M;
+	int f = 1;
+	
+	while(f<Y) {
+		f*=2;
+		if(f > Y) {
+			d = d * div(X, (Y-(f/2)), M);
+			d = d%M;
+			break;
+		}
+		d = d*d;
+		d %= M;
 	}
-	int p = 1;
-	//printf("Going left. ");
-	p *= div(X, Y/2, M);
-	//printf("Going right. ");
-	p *= div(X, Y-Y/2, M);
-	p %= M;
-	//printf("Returning %lld\n", (p));
-	return p;
+	
+	return d;
 }
