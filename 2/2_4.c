72a7b58214dfc349942f525f5eff6fff7b270793
Zubair Abid <zubairabid1999+github@gmail.com>
2017-10-06 15:45:58 +0530

    Close to solution for 2_4

--- 2/2_4.c
+++ 2/2_4.c
@@ -1,27 +1,31 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <math.h>
 
 long long int div(long long int, long long int, long long int);
 
-int main() {
-	long long int T, X, Y, M, i = 0;
+int main(void) {
+	long long int X, Y, M, T, i;
 	scanf("%lld", &T);
+	
 	for(i = 0; i < T; i++) {
+		//printf("Working on test case %lld\n", (i+1));
 		scanf("%lld %lld %lld", &X, &Y, &M);
-		printf("%lld\n", div(X, M, Y));
+		printf("%lld\n", div(X, Y, M));
 	}
-    return 0;
+	return 0;
 }
 
-long long int div(long long int num, long long int mod, long long int pow) {
-	if(pow == 1) {
-		return num%mod;
+long long int div(long long int X, long long int Y, long long int M) {
+	//printf("%lld p %lld mod %lld\n", X, Y, M);
+	if(Y==1) {
+	//	printf("(Base) Returning %lld\n", (X&M));
+		return (X%M);
 	}
-	printf("at power %lld\n", pow);
-	return ((num%mod)*div(num, mod, pow-1))%mod;
+	int p = 1;
+	//printf("Going left. ");
+	p *= div(X, Y/2, M);
+	//printf("Going right. ");
+	p *= div(X, Y-Y/2, M);
+	p %= M;
+	//printf("Returning %lld\n", (p));
+	return p;
 }
-
-// (a1.a2.a3.....an)%n = ((a1%n).(a2%n).(a3%n).....(an%n))%n
-// (a.b)%n = ((a%n).(b%n))%n
-// return ((a%n)*div(p))
