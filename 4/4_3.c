c35491e364ee282093a9c7488af977a40cfaeec3
zubairabid <zubairabid1999+github@gmail.com>
2017-11-06 08:39:28 +0530

    About to start with 3 properly, maybe

--- 4/4_3.c
+++ 4/4_3.c
@@ -1,17 +1,76 @@
 #include <stdio.h>
 
+//unsigned long long int t = 11111111110000000000;
+unsigned long long int ar[1000000];
+int c = 0;
+int track = 0;
+
+int generate(long long int N) {
+	if(N==0 && c==0) {
+		//ar[track] = ar[track]*10 + 0;
+		printf("Reset\n");
+		track++;
+		ar[track] = ar[track-1];
+		ar[track] /= 10;
+		c = -1;
+		return 1;
+	}
+	else if (N==0 && c!=0) {
+		//c=0;
+		ar[track]/=10;
+		return 0 ;
+	}
+
+	printf("N: %lld c:%d (-N/2):%lld\n", N, c, ((N/2)*-1));
+
+	if(c < 0) {
+		ar[track] = ar[track]*10 + 0;
+		//track++;
+		c++;
+		printf("Closing bracket\t\t%llu\n", ar[track]);
+		if(generate(N-1) == 0) {
+			c--;
+			c--;
+			ar[track] /= 10;
+		}
+	}
+	if (c >= ((N/2))*-1){
+		ar[track] = ar[track]*10 + 1;
+		//track++;
+		c--;
+		printf("Opening bracket\t\t%llu\n", ar[track]);
+		if(generate(N-1) == 0) {
+			c++;
+			c++;
+			ar[track] /= 10;
+		}
+	}
+	ar[track] /= 10;
+}
+
 int main() {
 	
+	//printf("%llu\n", t);
+	
+	long long int i, j, k, T, N;
+	 
+	scanf("%lld", &N);
+	
+	generate(2*N);
+
+	for(i = 0; i < track+1; i++) {
+		printf("%llu\n", ar[i]);
+	}
 
 	/*
 	long long int i, j, k, T, N;
- 
+	 
 	scanf("%lld", &N);
-
+	
 	for(i = 0; i < N; i++) {
 		
 	}
-
+	
 	printf("%lld\n", N);
 	*/
 
---
+++ 4/4_5.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+long long int loc[100][2];
+int filled=0;
+
+int main() {
+	long long int abx, aby, i, j, k, T, N, M, Q, x, y;
+ 	char eh[100]="";
+	scanf("%lld %lld", &N, &M);
+	scanf("%lld", &Q);
+
+	for(i = 0; i < Q; i++) {
+		scanf("%s", eh);
+		scanf("%lld %lld", &x, &y);
+
+		if(strcmp(eh, "Add") == 0) {
+			loc[filled][0] = x;
+			loc[filled++][1] = y;
+		}
+		else if(strcmp(eh, "Find") == 0) {
+			if(filled == 0) {
+				//-1
+				printf("-1\n");
+			}
+			else {
+				k = 20000;
+				for(j = 0; j < filled; j++) {
+					abx = loc[j][0] - x;
+					aby = loc[j][1] - y;
+					abx = abx>0?abx:-abx;
+					aby = aby>0?aby:-aby;
+					if(abx+aby < k) {
+						k = abx+aby;
+					}
+				}
+				printf("%lld\n", k);
+			}
+		}
+		else {
+			printf("What the fuhcs\n");
+		}
+
+	}
+
+
+
+	/*
+	long long int i, j, k, T, N;
+ 
+	scanf("%lld", &N);
+
+	for(i = 0; i < N; i++) {
+		
+	}
+
+	printf("%lld\n", N);
+	*/
+
+	return 0;
+}
