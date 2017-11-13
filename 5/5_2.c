94ad16c12a3716e6a11320a80978b11d1c0d3935
zubairabid <zubairabid1999+github@gmail.com>
2017-11-13 21:55:22 +0530

    Enabling 5

---
+++ 5/5_2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+//#include <string.h>
+//#include <math.h>
+
+long long int A[10000007];
+
+int main() {
+	//defvals
+	long long int i, j, k, T, N;
+
+	scanf("%lld", &N);
+
+	for(i = 0; i < N; i++) {
+		scanf("%lld", &A[i])	
+	}
+
+		
+
+	/*
+	default constructs
+	
+	long long int i, j, k, T, N;
+
+	//Input into N
+	scanf("%lld", &N);
+
+	
+	//Loop from 0 -- > N-1
+	for(i = 0; i < N; i++) {
+		
+	}
+	
+	Print statement
+	printf("%lld\n", N);
+	
+	*/
+	return 0;
+}
---
+++ 5/temp.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+//#include <string.h>
+//#include <math.h>
+
+int main() {
+	//defvals
+	long long int i, j, k, T, N;
+
+	scanf("%lld", &N);
+
+	/*
+	default constructs
+	
+	long long int i, j, k, T, N;
+
+	//Input into N
+	scanf("%lld", &N);
+
+	
+	//Loop from 0 -- > N-1
+	for(i = 0; i < N; i++) {
+		
+	}
+	
+	Print statement
+	printf("%lld\n", N);
+	
+	*/
+	return 0;
+}
