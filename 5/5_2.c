b66b8635e1f20289d667aa3518de132139116618
zubairabid <zubairabid1999+github@gmail.com>
2017-11-16 03:44:54 +0530

    2 is TLE. Fixing

--- 5/5_2.c
+++ 5/5_2.c
@@ -6,6 +6,7 @@ long long int A[1000007];
 long long int track[1000007];
 
 int main() {
+	/*
 	//defvals
 	long long int i, j, k=1, M, T, N, tr = 0, c = -1, max = 0;
 
@@ -71,5 +72,64 @@ int main() {
 	printf("%lld\n", N);
 	
 	*/
+
+	//default constructs	
+	long long int i, j, k, T, N, M, c = 0, length = 0, max = 2<<31, change;
+	//Input into N, M. N is number of inputs, M the number of flippables
+	scanf("%lld %lld", &N, &M);
+	
+	//Loop from 0 -- > N-1 to input into array
+	for(i = 0; i < N; i++) {
+		scanf("%lld", &A[i]);		
+	}
+	
+	i = 0;
+	c = 0;
+	length = 0;
+	max = 2<< 31;
+	// possible backtracking, but still theoretically 2N at max.
+	while(i < N) {
+		if(A[i] == 1) {
+			;
+		}
+		else {
+			c++;
+			//TODO: set the change variable
+			if(c == 1) {
+				change = i;
+			}
+		}
+
+		if(c > M) { // when the number of flipped bits is more than accounted for, 
+
+			if(length > max) { // if length is max evah, use it
+				max = length;
+			}
+			// since we don't need, reset length, flip counter, i.
+			length = 0; 
+			i = change + 1;
+			c = 0;
+			continue; // we don't want an accidental i++ again.
+		}
+		else {
+			length++; //yeh obvio
+		}
+
+		if(i == N-1) {
+			//also, accounting for last minute (i == N-1)
+			if(length > max) {
+				max = length;
+			}
+			break;
+		}
+
+		i++; //don't forget to
+	}
+	
+	printf("%lld\n", max);
+
+	//Print statement
+	//printf("%lld\n", N);
+	
 	return 0;
 }
---
+++ 5/out2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+print("1000000 500000")
+for i in range(1000000):
+	print("0", end=' ')
