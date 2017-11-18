00747468bdb7116ed750d2d62a050c533e828aa4
zubairabid <zubairabid1999+github@gmail.com>
2017-11-19 05:08:59 +0530

    motherfucking 3

--- 5/5_3.c
+++ 5/5_3.c
@@ -21,7 +21,7 @@ int main() {
 
 	for(j = 1; j <= N; j++) { // iterating through every element
 
-		for(i = 1; i <=j; i++) { // iterating through level foreach el ////////////////////////////////////////
+		for(i = 1; i <= j; i++) { // iterating through level foreach el ////////////////////////////////////////
 			if(i >= K && i == j) {
 				dp[i][j] = 0; //np
 				continue;
@@ -31,7 +31,48 @@ int main() {
 			}
 			else {
 				count[i][j] = 1;
+				if(j!=N) {
+				T = dp[i-1][MAX_2];
+				T = T + ar[j];
+				dp[i][MAX_2] = dp[i][MAX_1];
+				dp[i][MAX_1] = dp[i][MAX_];
+				if(T > dp[i][j]) {	
+					dp[i][j] = T;
+				}
+				
+				if(T > dp[i][MAX_]) {
+					dp[i][MAX_] = T;
+				}	
+
+				if(T > answer) {
+					answer = T;
+				}
+		/*		printf(" \t \t");
+			for(temp = 1; temp <= N; temp++) {
+				printf("%lld\t", ar[temp]);
 			}
+			printf("\n \t");
+			for(temp = 0; temp <= N; temp++)
+			{
+				printf("%lld\t", temp);
+			}
+			printf("\n");
+			for(temp = 0; temp <= N; temp++) {
+				printf("%lld\t", temp);
+				for(t2 = 0; t2 <= N; t2++) {
+					printf("%lld\t", dp[temp][t2]);
+				}	
+				printf("\t%lld\t%lld\t%lld", dp[temp][MAX_2], dp[temp][MAX_1], dp[temp][MAX_]);
+				printf("\n \t");
+				for(t2 = 0; t2 <= N; t2++) {
+					printf("%lld\t", count[temp][t2]);
+				}
+				printf("\n");
+			}
+			printf("------------------------------\n");	*/
+				continue;}
+			}
+			
 			if(count[i][j] % K != 0) {
 				T = dp[i-1][MAX_1];
 			}
@@ -41,7 +82,12 @@ int main() {
 			T = T + ar[j];
 			dp[i][MAX_2] = dp[i][MAX_1];
 			dp[i][MAX_1] = dp[i][MAX_];
-
+			/*
+			t2 = ((((N/K)-1)/1)+1);
+			if((i!=1) && ((t2*(K-1))+t2-1 > j)) {
+				T = 1;
+			}
+			*/
 			if(T > dp[i][j]) {	
 				dp[i][j] = T;
 			}
@@ -54,28 +100,32 @@ int main() {
 				answer = T;
 			}
 
-
-		printf(" \t \t");
-		for(temp = 1; temp <= N; temp++) {
-			printf("%lld\t", ar[temp]);
-		}
-		printf("\n \t");
-		for(temp = 0; temp <= N; temp++)
-		{
-			printf("%lld\t", temp);
-		}
-		printf("\n");
-		for(temp = 0; temp <= N; temp++) {
-			printf("%lld\t", temp);
-			for(t2 = 0; t2 <= N; t2++) {
-				printf("%lld\t", dp[temp][t2]);
+/*
+			printf(" \t \t");
+			for(temp = 1; temp <= N; temp++) {
+				printf("%lld\t", ar[temp]);
 			}
-			printf("\t%lld\t%lld\t%lld", dp[temp][MAX_2], dp[temp][MAX_1], dp[temp][MAX_]);
+			printf("\n \t");
+			for(temp = 0; temp <= N; temp++)
+			{
+				printf("%lld\t", temp);
+			}
 			printf("\n");
-		}
-		printf("------------------------------\n");
+			for(temp = 0; temp <= N; temp++) {
+				printf("%lld\t", temp);
+				for(t2 = 0; t2 <= N; t2++) {
+					printf("%lld\t", dp[temp][t2]);
+				}	
+				printf("\t%lld\t%lld\t%lld", dp[temp][MAX_2], dp[temp][MAX_1], dp[temp][MAX_]);
+				printf("\n \t");
+				for(t2 = 0; t2 <= N; t2++) {
+					printf("%lld\t", count[temp][t2]);
+				}
+				printf("\n");
+			}
+			printf("------------------------------\n");
 		
-		
+*/		
 		}
 	}
 	if(answer < 0) {
