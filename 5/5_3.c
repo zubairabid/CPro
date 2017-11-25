0ba91f913d0f973c2fbcc7b85373fa1f2c294b20
zubairabid <zubairabid1999+github@gmail.com>
2017-11-25 15:15:29 +0530

    bringing it up to date

--- 5/5_3.c
+++ 5/5_3.c
@@ -1,137 +1,12 @@
 #include <stdio.h>
 //#include <string.h>
 //#include <math.h>
-//#include <stdlib.h>
-long long int ar[5100];
-long long int dp[5100][5100]; // 5009 stores n_max, 5008 n-1, 5007 n-2
-long long int count[5100][5100];
-#define MAX_ 5009
-#define MAX_1 5008
-#define MAX_2 5007
-
 
 int main() {
 	//defvals
-	long long int i, j, T, N, K, answer = 1<<31, temp, t2;
-	scanf("%lld %lld", &N, &K);
+	long long int i, j, k, T, N;
 
-	for(i = 1; i <= N; i++) {
-		scanf("%lld", &ar[i]);	
-	}
-
-	for(j = 1; j <= N; j++) { // iterating through every element
-
-		for(i = 1; i <= j; i++) { // iterating through level foreach el ////////////////////////////////////////
-			if(i >= K && i == j) {
-				dp[i][j] = 0; //np
-				continue;
-			}
-			if(dp[i-1][MAX_1] == dp[i-1][j-1] && count[i-1][j-1] < K) {
-				count[i][j] = count[i-1][j-1] + 1;
-			}
-			else {
-				count[i][j] = 1;
-				if(j!=N) {
-				T = dp[i-1][MAX_2];
-				T = T + ar[j];
-				dp[i][MAX_2] = dp[i][MAX_1];
-				dp[i][MAX_1] = dp[i][MAX_];
-				if(T > dp[i][j]) {	
-					dp[i][j] = T;
-				}
-				
-				if(T > dp[i][MAX_]) {
-					dp[i][MAX_] = T;
-				}	
-
-				if(T > answer) {
-					answer = T;
-				}
-				printf(" \t \t");
-			for(temp = 1; temp <= N; temp++) {
-				printf("%lld\t", ar[temp]);
-			}
-			printf("\n \t");
-			for(temp = 0; temp <= N; temp++)
-			{
-				printf("%lld\t", temp);
-			}
-			printf("\n");
-			for(temp = 0; temp <= N; temp++) {
-				printf("%lld\t", temp);
-				for(t2 = 0; t2 <= N; t2++) {
-					printf("%lld\t", dp[temp][t2]);
-				}	
-				printf("\t%lld\t%lld\t%lld", dp[temp][MAX_2], dp[temp][MAX_1], dp[temp][MAX_]);
-				printf("\n \t");
-				for(t2 = 0; t2 <= N; t2++) {
-					printf("%lld\t", count[temp][t2]);
-				}
-				printf("\n");
-			}
-			printf("------------------------------\n");	
-				continue;}
-			}
-			
-			if(count[i][j] % K != 0) {
-				T = dp[i-1][MAX_1];
-			}
-			else {
-				T = dp[i-1][MAX_2];
-			}
-			T = T + ar[j];
-			dp[i][MAX_2] = dp[i][MAX_1];
-			dp[i][MAX_1] = dp[i][MAX_];
-			/*
-			t2 = ((((N/K)-1)/1)+1);
-			if((i!=1) && ((t2*(K-1))+t2-1 > j)) {
-				T = 1;
-			}
-			*/
-			if(T > dp[i][j]) {	
-				dp[i][j] = T;
-			}
-			
-			if(T > dp[i][MAX_]) {
-				dp[	i][MAX_] = T;
-			}
-
-			if(T > answer) {
-				answer = T;
-			}
-
-
-			printf(" \t \t");
-			for(temp = 1; temp <= N; temp++) {
-				printf("%lld\t", ar[temp]);
-			}
-			printf("\n \t");
-			for(temp = 0; temp <= N; temp++)
-			{
-				printf("%lld\t", temp);
-			}
-			printf("\n");
-			for(temp = 0; temp <= N; temp++) {
-				printf("%lld\t", temp);
-				for(t2 = 0; t2 <= N; t2++) {
-					printf("%lld\t", dp[temp][t2]);
-				}	
-				printf("\t%lld\t%lld\t%lld", dp[temp][MAX_2], dp[temp][MAX_1], dp[temp][MAX_]);
-				printf("\n \t");
-				for(t2 = 0; t2 <= N; t2++) {
-					printf("%lld\t", count[temp][t2]);
-				}
-				printf("\n");
-			}
-			printf("------------------------------\n");
-		
-		
-		}
-	}
-	if(answer < 0) {
-		answer = 0;
-	}
-	printf("%lld\n", answer);
+	scanf("%lld", &N);
 
 	/*
 	default constructs
@@ -153,3 +28,35 @@ int main() {
 	*/
 	return 0;
 }
+/*
+
+dp[n][k] = max(dp[n-1][k-1], dp[n-1][k])
+
+N = 5, k = 3
+6 10 8 4 2: --> index from 1
+j = 2, i = 1
+6
+j = 1, i = 2
+6  
+   16
+j = 0, i = 3
+
+if(a[i] < 0)
+	skip;
+if(j == 0)
+	fringe 
+dp[i][j] = max(a[i] + dp[i-1][j-1], dp[i-1][k])
+
+6 10 8 4 2
+
+6  10 8  4  2
+6  16 18 18 18
+6  16 
+
+
+
+
+
+
+
+*.
